8511c96a8199c3e9b9cd6dd2f00ccbb6116b8f2f
Fernando Alves <fernandoasfj@gmail.com>
2013-01-04 14:48:57 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurvivorBus::Application.config.secret_token = '8490faf8e52b946b902a44d2ac43d4e6f91d8f14083037ea316eced8b186c84e026be55beaa7a53354226d7050d808e3b0f725065427aeaba3d22fa17d860682'
